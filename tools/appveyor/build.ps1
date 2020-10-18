346fa988e59bc06cbfc0f423dd082aa0a37029af
James <11001588+MCterra10@users.noreply.github.com>
2020-10-18 12:47:04 +1100

    Update build.ps1

--- tools/appveyor/build.ps1
+++ tools/appveyor/build.ps1
@@ -1,5 +1,5 @@
 if(!(Test-Path -Path "C:/byond")){
-    bash tools/appveyor/download_byond.sh 2>&1
+    bash tools/appveyor/download_byond.sh
     [System.IO.Compression.ZipFile]::ExtractToDirectory("C:/byond.zip", "C:/")
     Remove-Item C:/byond.zip
 }
