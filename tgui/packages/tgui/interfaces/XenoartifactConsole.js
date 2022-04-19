7fe0bcd2ccfd9de5532715f0a9429fc2c519e324
Racc-Off <Hisphilosophy.comics@gmail.com>
2022-04-19 14:16:59 +1000

    Update XenoartifactConsole.js

--- tgui/packages/tgui/interfaces/XenoartifactConsole.js
+++ tgui/packages/tgui/interfaces/XenoartifactConsole.js
@@ -1,7 +1,7 @@
 import { map, toArray } from 'common/collections';
 import { classes } from 'common/react';
 import { useBackend } from '../backend';
-import {Box, Tabs, Section, Flex, Button, BlockQuote, Icon, Collapsible, AnimatedNumber} from '../components';
+import { Box, Tabs, Section, Flex, Button, BlockQuote, Icon, Collapsible, AnimatedNumber } from '../components';
 import { formatMoney } from '../format';
 import { Window } from '../layouts';
 
